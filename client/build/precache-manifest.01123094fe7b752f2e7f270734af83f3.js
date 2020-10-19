72c8b8019a5b42af3d780de096d11d7a824b19ad
Alanna Celentano <celentanoad91@gmail.com>
2020-10-18 22:14:37 -0500

    deploy

---
+++ client/build/precache-manifest.01123094fe7b752f2e7f270734af83f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02dd47a3d05d7df8e63e52c9d04c5d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e568fe57f84fd07e4f2f",
+    "url": "/static/css/main.68241033.chunk.css"
+  },
+  {
+    "revision": "8d5a674bdf508b444480",
+    "url": "/static/js/2.7200872f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7200872f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e568fe57f84fd07e4f2f",
+    "url": "/static/js/main.93d7ac8e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
