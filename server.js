6675474cd874affad610127d2570412cc200a5d6
Alanna Celentano <celentanoad91@gmail.com>
2020-10-18 22:46:38 -0500

    fix server.js

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ app.use(cors())
 //Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // API routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
